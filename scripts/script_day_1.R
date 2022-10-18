3618821595f67c21efbe232deb43db8e8b5768ba
Mark Andrews <mjandrews.org@gmail.com>
2022-10-18 15:59:59 +0100

    Update script

---
+++ scripts/script_day_1.R
@@ -0,0 +1,9 @@
+library(priorexposure)
+
+# likelihood function for the case of 
+# 139 heads in 250 flips
+bernoulli_likelihood(n = 250, m = 139)
+
+# likelihood function for the case of 
+# 14 heads in 25 flips
+bernoulli_likelihood(n = 25, m = 14)
